5ea373ae896b570ca639a102a9d7f78c327236dd
CHUANG YU TING <tina92350tina92350@gmail.com>
2021-07-28 14:54:04 +0800

    revise

--- lstm_yt.py
+++ lstm_yt.py
@@ -31,18 +31,17 @@ if '__main__' == __name__:
     files = os.listdir('./month_predict_input')
     cols = pickle.load(open('./selected_cols_for_total_data.pkl', 'rb'))
     cols.append('Date')
-    # cols = [s for s in cols if s not in ['y_updown', 'y_percent', 'y_point']]
 
-    full_x_array = np.empty((0,len(cols)))
-    full_y_array = np.empty((0,1))
+    cols = [i for i in cols if i not in ['y_updown', 'y_percent', 'y_point']]
 
-    for x in files:
+    all_x = np.empty([0, 48])
+    all_y = []
+
+    for x in files[:3]:
         with open(f'./month_predict_input/{x}' , 'rb') as f:
             stock_code = x.replace('_month_fin_merge.pkl', '')
             predict_content = pickle.load(f)
             predict_content = predict_content.reset_index().rename(columns={'index': 'Date'})
-
-
             dates =  predict_content.Date
 
             for year in range(2004, 2021):
@@ -51,37 +50,17 @@ if '__main__' == __name__:
                         count = dates.loc[(dates>=datetime(year, month, 1)) & (dates<datetime(year, month+1, 1))].index
                     else:
                         count = dates.loc[(dates>=datetime(year, 12, 1)) & (dates<datetime(year+1, 1, 1))].index
-                    wanted = predict_content.iloc[count][cols].values
+                    wanted = predict_content.iloc[count]
 
                     if wanted.shape[0] != 0 and wanted.shape[0] != 24:
+                        y = wanted['y_point'].values[-1] - wanted['y_point'].values[0]
+                        wanted = wanted[cols].values
                         pad = np.repeat(wanted[-1].reshape(1, -1), 24-wanted.shape[0], axis=0)
                         wanted = np.concatenate((wanted, pad), axis=0)
-                        print(wanted.shape)
 
-    #             if date.day<pre_day:
-    #                 full_x_array = np.append(full_x_array,predict_content[predict_content['Date']<date][predict_content['Date']>=start_date][all_col].values)
-    #                 for null_date in range(max_month_day-one_month_day):
-    #                     full_x_array = np.append(full_x_array,predict_content[predict_content['Date'] == full_x_array[-1]][all_col])
-    #                 start_date = date
-    #                 pre_day = count_day
-    #                 count_day = count_day + one_month_day
-    #                 new_point = predict_content['close'][count_day]
-    #                 y_date = predict_content['Date'][pre_day]
-    #                 if new_point-pre_point>0:
-    #                     full_y_array = np.append(full_y_array,1)
-    #                 elif new_point-pre_point==0:
-    #                     full_y_array = np.append(full_y_array,0)
-    #                 else:
-    #                     full_y_array = np.append(full_y_array,-1)
-    #                 full_y_array = np.append(full_y_array,y_date)
-    #                 pre_point = new_point
-    #                 one_month_day = 1
-    #                 pre_day = date.day
-    #             else:
-    #                 one_month_day = one_month_day + 1
-    #             pre_day = date.day
-    # print(full_x_array)
-    # print(full_y_array)
-    # pickle.dump(full_x_array,open(f'./new_monthfin_lstm_x_input.pkl','wb'))
-    # pickle.dump(full_y_array,open(f'./new_monthfin_lstm_y_input.pkl','wb'))
-    #
+                        all_x = np.concatenate((all_x, wanted), axis=0)
+                        all_y.append(y)
+    all_x = all_x.reshape(-1, 24, 48)
+    print(all_x.shape)
+    print(len(all_y))
+
