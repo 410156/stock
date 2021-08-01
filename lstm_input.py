9c9c034cdeaf657ee84b5b2b81cfb62ba596ef34
qqqqqq <410156@stu.nknusk.kh.edu.tw>
2021-08-01 20:16:51 +0800

    modify

--- lstm_input.py
+++ lstm_input.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 
 
-def combine_files(files,all_col,full_x_array,full_y_array):
+def combine_files(files,all_col,full_x,full_y):
 
 
     for x in files:
@@ -31,28 +31,25 @@ def combine_files(files,all_col,full_x_array,full_y_array):
                 #若上一個回圈的日比這個回圈的日大代表已經到下一個月了,且date為新月份第一個日期
                 if date.day<pre_day:
                     print('-')
-                    #把上一個月份的日期區間資料append到full_x_array
-                    full_x_array = np.append(full_x_array,stock_data[stock_data['Date']<date][stock_data['Date']>=start_date][all_col].values)
+                    #把上一個月份的日期區間資料append到full_x
+                    full_x = np.concatenate((full_x,stock_data[stock_data['Date']<date][stock_data['Date']>=start_date][all_col].values),axis=0)
                     #用這個月最後一開盤日數據補足剩餘不滿24天的數據
                     for null_date in range(max_month_day-one_month_day):
 
-                                                   #! full_x_array 是array,   後面這邊是dataframe, 這樣np.append沒問題嗎(?)
-                                                   #把1個row ,48個col的dataframe append到array後使full_x_array增加48個一維資料 
-                        print(full_x_array.shape) #! 這個full_x_array的維度只有一維? 為什麼不是48個col所以有48維?
-                        full_x_array = np.append(full_x_array,stock_data[stock_data['Date'] == full_x_array[-1]][all_col])
-                        print(full_x_array.shape)
-                                       #新月份第一個開盤日的收盤價
-                    new_point = stock_data[stock_data['Date'] == date]['close'].values
-                    if new_point-pre_point>0:
-                        full_y_array = np.append(full_y_array,1)
-                    elif new_point-pre_point==0:
-                        full_y_array = np.append(full_y_array,0)
-                    else:
-                        full_y_array = np.append(full_y_array,-1)
-                    #把新月份減舊月份收盤價放到舊月份第一個開盤日
-                    full_y_array = np.append(full_y_array,start_date)
+                                                   #! full_x 是array,   後面這邊是dataframe, 這樣np.append沒問題嗎(?)
+                        print(full_x.shape) #! 這個full_x的維度只有一維? 為什麼不是48個col所以有48維?
+                        full_x = np.concatenate((full_x,stock_data[stock_data['Date'] == full_x[-1][-1]][all_col]),axis=0)
+                        print(full_x.shape)
 
-                    #y_date = start_date    #! y_date = start_date = date ? 那為什麼要分成這麼多變數, 不延用date一個變數就好?                    
+                    #新月份第一個開盤日的收盤價
+                    new_point = stock_data[stock_data['Date'] == date]['close'].values
+                    #把新月份減舊月份收盤價放到舊月份第一個開盤日
+                    if new_point-pre_point>0:
+                        full_y = full_y.append({'y':1,'Date':start_date},ignore_index=True)
+                    elif new_point-pre_point==0:
+                        full_y = full_y.append({'y':0,'Date':start_date},ignore_index=True)
+                    else:
+                        full_y = full_y.append({'y':-1,'Date':start_date},ignore_index=True)
                     #更新start_date
                     start_date = date
 
@@ -62,16 +59,17 @@ def combine_files(files,all_col,full_x_array,full_y_array):
                 else:
                     one_month_day = one_month_day + 1
                 pre_day = date.day
-                #if count == 20:
-                #    exit()
+                if count == 20:
+                    exit()
 
                 print(pre_day)
                 print('-'*30)
 
-    print(full_x_array.shape)
-    print(full_y_array.shape)
+    print(full_x.shape)
+    print(full_y.shape)
+    
 
-    return full_x_array, full_y_array
+    return full_x, full_y
 
 
 
@@ -82,11 +80,13 @@ if '__main__' == __name__:
     all_col.append('Date')
     all_col = [s for s in all_col if s != 'y_updown' and s != 'y_percent' and s != 'y_point']
 
-    full_x_array = np.empty((0,len(all_col)))
-    full_y_array = np.empty((0,1))
+    full_x = np.empty((0,len(all_col)))
+    #full_y = np.empty((0,1))
+    full_y = pd.DataFrame(columns = ['y','Date'])
 
-    full_x_array,full_y_array = combine_files(files,all_col,full_x_array,full_y_array)
+    full_x,full_y = combine_files(files,all_col,full_x,full_y)
+    full_x = pd.DataFrame(full_x,columns = all_col)
 
-    pickle.dump(full_x_array,open(f'./monthfin_lstm_x_input.pkl','wb'))
-    pickle.dump(full_y_array,open(f'./monthfin_lstm_y_input.pkl','wb'))
+    pickle.dump(full_x,open(f'./monthfin_lstm_x_input.pkl','wb'))
+    pickle.dump(full_y,open(f'./monthfin_lstm_y_input.pkl','wb'))
 
